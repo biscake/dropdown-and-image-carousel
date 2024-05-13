841a214a5ed30b5dbf893120fb58f3b3bda087b6
biscake <leonut.ljh@gmail.com>
2024-05-13 12:36:09 +0800

    Added dropdown menu Added image carousel

--- src/index.js
+++ src/index.js
@@ -1,1 +1,6 @@
-import "./style.css";
+import "./css/reset.css";
+import "./css/style.css";
+import addDropdownEventListener from "./script/toggleDropdown";
+import "./script/imgCarousel";
+
+addDropdownEventListener();
